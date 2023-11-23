1b54fa59f21ed7bf7092668b9f8e2ce84bd0acaf
Duncan Temple Lang <duncan@r-project.org>
2023-11-22 18:10:07 -0800

    removed a bunch of code from the old index mechanism for formals, body, top-level

--- R/indexing.R
+++ R/indexing.R
@@ -1,48 +1,14 @@
 mkIndexPath =
 function(idx, type) 
 {
-#            browser()
     klass = switch(type,
                    "body" = "BodyIndex",
                    "formals" = "FormalsIndex",
                    "BasicIndex")
     
-#   idx = c(if(is.na(type) || type == "")
-#               NA
-#           else if(type == "body")
-#               1L
-#           else 0L,
-#           idx)
-
     class(idx) = c(klass, "IndexPath")
     idx
 }
-
-if(FALSE) 
-getIndexObj = 
-function(ast, idx, type = NA, dropSelf = TRUE)
-{    
-    obj = if(is.na(type)) {
-              ty = idx[1]
-              idx = idx[-1]
-              if(is.na(ty))
-                  ast
-              else switch(as.character(ty),
-                     "0" = formals(ast),
-                     "1" = body(ast),
-                     ast)
-          } else               
-              switch(type,
-                     body = body(ast),
-                     formals = formals(ast),
-                     ast)
-
-    if(dropSelf)
-        idx = idx[ - length(idx)]
-
-    list(obj = obj, idx = idx)
-}
-
 
 getIndexObj =
     #
@@ -74,16 +40,5 @@ function(idx, type = NA)
            FormalsIndex = "formals",
            ""
            )
-#  if(is.na(type)) {
-#      ty = idx[1]
-#       switch(as.character(ty),
-#             "0" = "formals",
-#             "1" = "body",
-#              "")
-#  } else               
-#      switch(type,
-#             body = "body",
-#             formals = "formals",
-#             "")    
 }
 
