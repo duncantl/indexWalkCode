29103d1df9cf1457143656c4c3f5dd26281e9b58
Duncan Temple Lang <duncan@r-project.org>
2023-11-22 19:40:27 -0800

    getAncestors.  Fix the looping over the indices. And add self = FALSE parameter

--- R/treeAccessors.R
+++ R/treeAccessors.R
@@ -26,19 +26,19 @@ getAncestors =
     # Need to enhance to handle when type is NA and the type is the first element
     # of idx. Same as getParent().
     #
-function(ast, idx, type = NA)
+function(ast, idx, self = FALSE, type = NA)
 {
     if(!inherits(idx, "IndexPath"))
         idx = mkIndexPath(idx, type)
     
-    tmp = getIndexObj(ast, idx)
+    tmp = getIndexObj(ast, idx, dropSelf = !self)
     
     orig = obj = tmp$obj
-    idx = tmp$idx
+#    idx = tmp$idx
     
-    ans = vector("list", length(idx))
-    for(i in seq(along.with = idx) )
-        ans[[i]] = obj = obj[[i]]
+    ans = vector("list", length(tmp$idx))
+    for(i in seq(along.with = tmp$idx) )
+        ans[[i]] = obj = obj[[ tmp$idx[i] ]]
 
     # add the body or parameter and then the ast itself.
     rev(c(ast, orig, ans))
@@ -46,7 +46,7 @@ function(ast, idx, type = NA)
 
 
 getSiblings =
-function(ast, idx, type = NA, before = TRUE)
+function(ast, idx, before = TRUE, type = NA)
 {
     if(!inherits(idx, "IndexPath"))
         idx = mkIndexPath(idx, type)
