4c9eb2c467972fd50c2ae6452f4fe0a931e71c09
Duncan Temple Lang <duncan@r-project.org>
2023-11-22 19:54:12 -0800

    change the order of the arguments to pred to put type last

--- tests/predParent.R
+++ tests/predParent.R
@@ -4,7 +4,7 @@ library(indexWalkCode)
 # Find the use of args in the call
 #  o2 = do.call(order, args)
 #
-pred2 = function(x, idx, type, ast) {
+pred2 = function(x, idx, ast, type) {
 #    print(x)
     isSymbol(x, "args") &&
         isCallTo(p <- getParent(ast, idx, type), "do.call") &&
