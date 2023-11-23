17aefc24891721cfbc51199dae3128b90a42d78a
Duncan Temple Lang <duncan@r-project.org>
2023-11-22 16:53:58 -0800

    predicate functions

--- R/predicates.R
+++ R/predicates.R
@@ -4,11 +4,25 @@ mkIsCallTo =
     # This captures fun and returns a function which can be used as the predicate
     # function in walkCode or indexWalkCode.
 function(fun)    
-    function(x, ...)  # idx, type, ast) 
-        is.call(x) && isSymbol(x[[1]], fun)
+    function(x, ...)  # idx, type, ast)
+        isCallTo(x, fun)
+
 
 isSymbol =
     # Copied from CodeAnalysis. Need to rationalize.
 function (x, sym) 
-is.name(x) && as.character(x) %in% sym
+   is.name(x) && as.character(x) %in% sym
 
+
+
+isCallTo =
+function (code, funName) 
+{
+   (is.call(code) || inherits(code, "call")) && isSymbol(code[[1]], funName)
+}
+
+isAssignTo =
+function (code, varName) 
+{
+   inherits(code, c("<-", "=")) && isSymbol(code[[2]], varName)
+}
